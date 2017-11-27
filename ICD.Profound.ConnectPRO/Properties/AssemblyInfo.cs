95b0e376b241e144ad4844564b5da015f5541de4
Chris Cameron <chris.cameron@wedoresi.com>
2017-11-27 15:03:35 -0500

    Adding initial project

---
+++ ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Profound.ConnectPRO")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ICD.Profound.ConnectPRO")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
