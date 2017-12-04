30a145b3a8e28cc05b044610b499bdcebf206734
Chris Cameron <chris.cameron@wedoresi.com>
2017-12-04 11:08:58 -0500

    Begin implementing option views

---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Views/Common/Options/AbstractOptionView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ICD.Connect.Panels;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common.Options;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Views.Common.Options
+{
+	public abstract class AbstractOptionView : AbstractView, IOptionView
+	{
+		public abstract event EventHandler OnButtonPressed;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="panel"></param>
+		/// <param name="theme"></param>
+		protected AbstractOptionView(ISigInputOutput panel, ConnectProTheme theme)
+			: base(panel, theme)
+		{
+		}
+	}
+}
