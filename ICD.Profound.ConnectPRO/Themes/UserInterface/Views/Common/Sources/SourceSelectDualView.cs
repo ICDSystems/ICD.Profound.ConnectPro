d1308fdb46bf6939b4449a95011e2644b233fc90
Chris Cameron <chris.cameron@wedoresi.com>
2017-12-04 15:24:19 -0500

    Implementing source select views

---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Views/Common/Sources/SourceSelectDualView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using ICD.Connect.Panels;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common.Sources;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Views.Common.Sources
+{
+	public sealed partial class SourceSelectDualView : AbstractSourceSelectView, ISourceSelectDualView
+	{
+		private readonly List<IReferencedSourceSelectView> m_ChildList;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="panel"></param>
+		/// <param name="theme"></param>
+		public SourceSelectDualView(ISigInputOutput panel, ConnectProTheme theme)
+			: base(panel, theme)
+		{
+			m_ChildList = new List<IReferencedSourceSelectView>();
+		}
+
+		/// <summary>
+		/// Returns child views for list items.
+		/// </summary>
+		/// <param name="factory"></param>
+		/// <param name="count"></param>
+		/// <returns></returns>
+		public override IEnumerable<IReferencedSourceSelectView> GetChildComponentViews(IViewFactory factory, ushort count)
+		{
+			return GetChildViews(factory, m_SourceList, m_ChildList, count);
+		}
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Views/Common/Sources/SourceSelectDualView.generated.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using ICD.Connect.Panels;
+using ICD.Connect.UI.Controls;
+using ICD.Connect.UI.Controls.Lists;
+using ICD.Connect.UI.Controls.Pages;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Views.Common.Sources
+{
+	public sealed partial class SourceSelectDualView
+	{
+		private VtProSubpage m_Subpage;
+		private VtProSubpageReferenceList m_SourceList;
+
+		/// <summary>
+		/// Instantiates the view controls.
+		/// </summary>
+		/// <param name="panel"></param>
+		/// <param name="parent"></param>
+		/// <param name="index"></param>
+		protected override void InstantiateControls(ISigInputOutput panel, IVtProParent parent, ushort index)
+		{
+			m_Subpage = new VtProSubpage(panel, parent)
+			{
+				DigitalVisibilityJoin = 110
+			};
+
+			m_SourceList = new VtProSubpageReferenceList(1, panel as IPanelDevice, m_Subpage)
+			{
+				MaxSize = 20,
+				DigitalJoinIncrement = 3,
+				AnalogJoinIncrement = 2,
+				SerialJoinIncrement = 3
+			};
+		}
+
+		/// <summary>
+		/// Gets the child controls.
+		/// </summary>
+		/// <returns></returns>
+		protected override IEnumerable<IVtProControl> GetChildren()
+		{
+			yield return m_Subpage;
+			yield return m_SourceList;
+		}
+	}
+}
