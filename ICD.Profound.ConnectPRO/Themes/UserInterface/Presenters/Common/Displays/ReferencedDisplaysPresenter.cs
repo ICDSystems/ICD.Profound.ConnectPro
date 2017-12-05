ac356a827d4547f1974b5e99098eff638ccf9c32
Chris Cameron <chris.cameron@wedoresi.com>
2017-12-05 11:14:07 -0500

    Begin implementing displays presenters

---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Displays/ReferencedDisplaysPresenter.cs
@@ -0,0 +1,22 @@
+﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.Common.Displays;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common.Displays;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Displays
+{
+	public sealed class ReferencedDisplaysPresenter : AbstractComponentPresenter<IReferencedDisplaysView>,
+	                                                  IReferencedDisplaysPresenter
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="nav"></param>
+		/// <param name="views"></param>
+		/// <param name="theme"></param>
+		public ReferencedDisplaysPresenter(INavigationController nav, IViewFactory views, ConnectProTheme theme)
+			: base(nav, views, theme)
+		{
+		}
+	}
+}
