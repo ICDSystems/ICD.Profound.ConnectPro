686ed26ea16bbc152f692e2415777479286d8e57
Chris Cameron <chris.cameron@wedoresi.com>
2017-12-06 13:19:52 -0500

    Implementing EndMeetingPresenter and StartMeetingPresenter

--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/EndMeetingPresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/EndMeetingPresenter.cs
@@ -1,4 +1,7 @@
-﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+﻿using System;
+using ICD.Common.Utils.EventArguments;
+using ICD.Profound.ConnectPRO.Rooms;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.Common;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common;
@@ -17,5 +20,79 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common
 			: base(nav, views, theme)
 		{
 		}
+
+		#region View Callbacks
+
+		/// <summary>
+		/// Subscribe to the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Subscribe(IEndMeetingView view)
+		{
+			base.Subscribe(view);
+
+			view.OnEndMeetingButtonPressed += ViewOnEndMeetingButtonPressed;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Unsubscribe(IEndMeetingView view)
+		{
+			base.Unsubscribe(view);
+
+			view.OnEndMeetingButtonPressed -= ViewOnEndMeetingButtonPressed;
+		}
+
+		/// <summary>
+		/// Called when the user presses the end meeting button.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="eventArgs"></param>
+		private void ViewOnEndMeetingButtonPressed(object sender, EventArgs eventArgs)
+		{
+			if (Room != null)
+				Room.IsInMeeting = false;
+		}
+
+		#endregion
+
+		#region Room Callbacks
+
+		/// <summary>
+		/// Subscribe to the room events.
+		/// </summary>
+		/// <param name="room"></param>
+		protected override void Subscribe(IConnectProRoom room)
+		{
+			base.Subscribe(room);
+
+			room.OnIsInMeetingChanged += RoomOnIsInMeetingChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the room events.
+		/// </summary>
+		/// <param name="room"></param>
+		protected override void Unsubscribe(IConnectProRoom room)
+		{
+			base.Unsubscribe(room);
+
+			room.OnIsInMeetingChanged -= RoomOnIsInMeetingChanged;
+		}
+
+		/// <summary>
+		/// Called when the room enters/exits a meeting.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="boolEventArgs"></param>
+		private void RoomOnIsInMeetingChanged(object sender, BoolEventArgs boolEventArgs)
+		{
+			bool show = Room != null && Room.IsInMeeting;
+			ShowView(show);
+		}
+
+		#endregion
 	}
 }
--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/StartMeetingPresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/StartMeetingPresenter.cs
@@ -1,4 +1,7 @@
-﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+﻿using System;
+using ICD.Common.Utils.EventArguments;
+using ICD.Profound.ConnectPRO.Rooms;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.Common;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common;
@@ -17,5 +20,79 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common
 			: base(nav, views, theme)
 		{
 		}
+
+		#region View Callbacks
+
+		/// <summary>
+		/// Subscribe to the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Subscribe(IStartMeetingView view)
+		{
+			base.Subscribe(view);
+
+			view.OnStartMeetingButtonPressed += ViewOnStartMeetingButtonPressed;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Unsubscribe(IStartMeetingView view)
+		{
+			base.Unsubscribe(view);
+
+			view.OnStartMeetingButtonPressed -= ViewOnStartMeetingButtonPressed;
+		}
+
+		/// <summary>
+		/// Called when the user presses the start meeting button.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="eventArgs"></param>
+		private void ViewOnStartMeetingButtonPressed(object sender, EventArgs eventArgs)
+		{
+			if (Room != null)
+				Room.IsInMeeting = true;
+		}
+
+		#endregion
+
+		#region Room Callbacks
+
+		/// <summary>
+		/// Subscribe to the room events.
+		/// </summary>
+		/// <param name="room"></param>
+		protected override void Subscribe(IConnectProRoom room)
+		{
+			base.Subscribe(room);
+
+			room.OnIsInMeetingChanged += RoomOnIsInMeetingChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the room events.
+		/// </summary>
+		/// <param name="room"></param>
+		protected override void Unsubscribe(IConnectProRoom room)
+		{
+			base.Unsubscribe(room);
+
+			room.OnIsInMeetingChanged -= RoomOnIsInMeetingChanged;
+		}
+
+		/// <summary>
+		/// Called when the room enters/exits a meeting.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="boolEventArgs"></param>
+		private void RoomOnIsInMeetingChanged(object sender, BoolEventArgs boolEventArgs)
+		{
+			bool show = Room != null && !Room.IsInMeeting;
+			ShowView(show);
+		}
+
+		#endregion
 	}
 }
