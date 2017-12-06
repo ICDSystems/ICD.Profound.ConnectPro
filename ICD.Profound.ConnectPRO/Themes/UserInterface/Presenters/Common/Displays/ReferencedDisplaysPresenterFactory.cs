cf27c5cdb49d13281f781aa173af744342ea664a
Chris Cameron <chris.cameron@wedoresi.com>
2017-12-06 17:16:41 -0500

    Begin implementing referenced presenter factories

---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Displays/ReferencedDisplaysPresenterFactory.cs
@@ -0,0 +1,33 @@
+﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.Common.Displays;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common.Displays;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Displays
+{
+	public sealed class ReferencedDisplaysPresenterFactory :
+		AbstractListItemFactory<object, IReferencedDisplaysPresenter, IReferencedDisplaysView>
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="navigationController"></param>
+		/// <param name="viewFactory"></param>
+		public ReferencedDisplaysPresenterFactory(INavigationController navigationController,
+		                                          ListItemFactory<IReferencedDisplaysView> viewFactory)
+			: base(navigationController, viewFactory)
+		{
+		}
+
+		/// <summary>
+		/// Binds the model and view to the presenter.
+		/// </summary>
+		/// <param name="model"></param>
+		/// <param name="presenter"></param>
+		/// <param name="view"></param>
+		protected override void BindMvpTriad(object model, IReferencedDisplaysPresenter presenter,
+											 IReferencedDisplaysView view)
+		{
+			presenter.SetView(view);
+		}
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Sources/ReferencedSourceSelectPresenterFactory.cs
@@ -0,0 +1,33 @@
+﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.Common.Sources;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common.Sources;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Sources
+{
+	public sealed class ReferencedSourceSelectPresenterFactory :
+		AbstractListItemFactory<object, IReferencedSourceSelectPresenter, IReferencedSourceSelectView>
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="navigationController"></param>
+		/// <param name="viewFactory"></param>
+		public ReferencedSourceSelectPresenterFactory(INavigationController navigationController,
+		                                              ListItemFactory<IReferencedSourceSelectView> viewFactory)
+			: base(navigationController, viewFactory)
+		{
+		}
+
+		/// <summary>
+		/// Binds the model and view to the presenter.
+		/// </summary>
+		/// <param name="model"></param>
+		/// <param name="presenter"></param>
+		/// <param name="view"></param>
+		protected override void BindMvpTriad(object model, IReferencedSourceSelectPresenter presenter,
+		                                     IReferencedSourceSelectView view)
+		{
+			presenter.SetView(view);
+		}
+	}
+}
