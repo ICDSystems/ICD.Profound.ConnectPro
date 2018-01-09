2960c4032c41d92d0ec919efe2f9f8c4ee990f65
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-09 14:36:09 -0500

    Removing unused code

--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/AbstractListItemFactory.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/AbstractListItemFactory.cs
@@ -7,9 +7,6 @@ using ICD.Common.Utils;
 using ICD.Common.Utils.Extensions;
 using ICD.Profound.ConnectPRO.Rooms;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
-#if !SIMPLSHARP
-using System.Reflection;
-#endif
 
 namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters
 {
