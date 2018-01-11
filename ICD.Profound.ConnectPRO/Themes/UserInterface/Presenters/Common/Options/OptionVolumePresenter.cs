95406d700aa1c0f8d26875a702521d44dc41e90a
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-11 15:56:55 -0500

    Volume option button opens volume presenter

--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Options/OptionVolumePresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Options/OptionVolumePresenter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using ICD.Common.Utils.EventArguments;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.Common;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.Common.Options;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Common.Options;
@@ -8,6 +10,13 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Options
 {
 	public sealed class OptionVolumePresenter : AbstractOptionPresenter<IOptionVolumeView>, IOptionVolumePresenter
 	{
+		private IVolumePresenter m_CachedMenu;
+
+		/// <summary>
+		/// Gets the menu for this button.
+		/// </summary>
+		private IVolumePresenter Menu { get { return m_CachedMenu ?? (m_CachedMenu = Navigation.LazyLoadPresenter<IVolumePresenter>()); } }
+
 		/// <summary>
 		/// Constructor.
 		/// </summary>
@@ -17,6 +26,7 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Options
 		public OptionVolumePresenter(INavigationController nav, IViewFactory views, ConnectProTheme theme)
 			: base(nav, views, theme)
 		{
+			Subscribe(Menu);
 		}
 
 		/// <summary>
@@ -26,7 +36,61 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Options
 		/// <param name="eventArgs"></param>
 		protected override void ViewOnButtonPressed(object sender, EventArgs eventArgs)
 		{
-			throw new NotImplementedException();
+			Menu.ShowView(!Menu.IsViewVisible);
 		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		public override void Dispose()
+		{
+			if (m_CachedMenu != null)
+				Unsubscribe(m_CachedMenu);
+
+			base.Dispose();
+		}
+
+		/// <summary>
+		/// Updates the view.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Refresh(IOptionVolumeView view)
+		{
+			base.Refresh(view);
+
+			view.SetSelected(Menu.IsViewVisible);
+		}
+
+		#region Navigation Callbacks
+
+		/// <summary>
+		/// Subscribe to the menu events.
+		/// </summary>
+		/// <param name="menu"></param>
+		private void Subscribe(IVolumePresenter menu)
+		{
+			menu.OnViewVisibilityChanged += MenuOnViewVisibilityChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the menu events.
+		/// </summary>
+		/// <param name="menu"></param>
+		private void Unsubscribe(IVolumePresenter menu)
+		{
+			menu.OnViewVisibilityChanged -= MenuOnViewVisibilityChanged;
+		}
+
+		/// <summary>
+		/// Called when the menu visibility changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="boolEventArgs"></param>
+		private void MenuOnViewVisibilityChanged(object sender, BoolEventArgs boolEventArgs)
+		{
+			RefreshIfVisible();
+		}
+
+		#endregion
 	}
 }
