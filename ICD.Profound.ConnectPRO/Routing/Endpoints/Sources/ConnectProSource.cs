fc7f37eb501b6edf401280fd7218308b1e322d27
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-12 10:53:14 -0500

    Initial commit of ConnectProSource

---
+++ ICD.Profound.ConnectPRO/Routing/Endpoints/Sources/ConnectProSource.cs
@@ -0,0 +1,34 @@
+﻿using ICD.Connect.Routing.Endpoints.Sources;
+using ICD.Connect.Settings.Core;
+
+namespace ICD.Profound.ConnectPRO.Routing.Endpoints.Sources
+{
+	public sealed class ConnectProSource : AbstractSource<ConnectProSourceSettings>
+	{
+		/// <summary>
+		/// Gets/sets the icon serial.
+		/// </summary>
+		public string Icon { get; set; }
+
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			Icon = null;
+		}
+
+		protected override void CopySettingsFinal(ConnectProSourceSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.Icon = Icon;
+		}
+
+		protected override void ApplySettingsFinal(ConnectProSourceSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			Icon = settings.Icon;
+		}
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Routing/Endpoints/Sources/ConnectProSourceSettings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Common.Utils.Xml;
+using ICD.Connect.Routing.Endpoints.Sources;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Profound.ConnectPRO.Routing.Endpoints.Sources
+{
+	public sealed class ConnectProSourceSettings : AbstractSourceSettings
+	{
+		private const string FACTORY_NAME = "ConnectProSource";
+
+		private const string ICON_ELEMENT = "Icon";
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the originator factory name.
+		/// </summary>
+		public override string FactoryName { get { return FACTORY_NAME; } }
+
+		/// <summary>
+		/// Gets the type of the originator for this settings instance.
+		/// </summary>
+		public override Type OriginatorType { get { return typeof(ConnectProSource); } }
+
+		/// <summary>
+		/// Gets/sets the icon serial.
+		/// </summary>
+		public string Icon { get; set; }
+
+		#endregion
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(ICON_ELEMENT, Icon);
+		}
+
+		/// <summary>
+		/// Loads the settings from XML.
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		[PublicAPI, XmlFactoryMethod(FACTORY_NAME)]
+		public static ConnectProSourceSettings FromXml(string xml)
+		{
+			ConnectProSourceSettings output = new ConnectProSourceSettings
+			{
+				Icon = XmlUtils.TryReadChildElementContentAsString(xml, ICON_ELEMENT)
+			};
+
+			ParseXml(output, xml);
+			return output;
+		}
+	}
+}
