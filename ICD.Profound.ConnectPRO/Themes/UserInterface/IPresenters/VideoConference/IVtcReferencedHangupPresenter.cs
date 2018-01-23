75498a083b5249a89be1704d6dcee656b1089f10
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-23 10:59:31 -0500

    Implementing VtcReferencedHangupPresenter

--- ICD.Profound.ConnectPRO/Themes/UserInterface/IPresenters/VideoConference/IVtcReferencedHangupPresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/IPresenters/VideoConference/IVtcReferencedHangupPresenter.cs
@@ -1,8 +1,14 @@
-﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.VideoConference;
+﻿using ICD.Connect.Conferencing.ConferenceSources;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.VideoConference;
 
 namespace ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.VideoConference
 {
 	public interface IVtcReferencedHangupPresenter : IPresenter<IVtcReferencedHangupView>
 	{
+		/// <summary>
+		/// Sets the conference source for this presenter.
+		/// </summary>
+		/// <value></value>
+		IConferenceSource ConferenceSource { get; set; }
 	}
 }
--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/VideoConference/VtcReferencedHangupPresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/VideoConference/VtcReferencedHangupPresenter.cs
@@ -1,4 +1,8 @@
-﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+﻿using System;
+using ICD.Common.Utils;
+using ICD.Common.Utils.EventArguments;
+using ICD.Connect.Conferencing.ConferenceSources;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.VideoConference;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.VideoConference;
@@ -8,6 +12,29 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.VideoConferenc
 	public sealed class VtcReferencedHangupPresenter : AbstractComponentPresenter<IVtcReferencedHangupView>,
 	                                                   IVtcReferencedHangupPresenter
 	{
+		private readonly SafeCriticalSection m_RefreshSection;
+		private IConferenceSource m_Source;
+
+		/// <summary>
+		/// Sets the conference source for this presenter.
+		/// </summary>
+		/// <value></value>
+		public IConferenceSource ConferenceSource
+		{
+			get { return m_Source; }
+			set
+			{
+				if (value == m_Source)
+					return;
+
+				Unsubscribe(m_Source);
+				m_Source = value;
+				Subscribe(m_Source);
+
+				RefreshIfVisible();
+			}
+		}
+
 		/// <summary>
 		/// Constructor.
 		/// </summary>
@@ -17,6 +44,92 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.VideoConferenc
 		public VtcReferencedHangupPresenter(INavigationController nav, IViewFactory views, ConnectProTheme theme)
 			: base(nav, views, theme)
 		{
+			m_RefreshSection = new SafeCriticalSection();
 		}
+
+		/// <summary>
+		/// Updates the view.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Refresh(IVtcReferencedHangupView view)
+		{
+			base.Refresh(view);
+
+			m_RefreshSection.Enter();
+
+			try
+			{
+				view.SetContactName(m_Source == null ? null : m_Source.Name);
+				view.SetContactNumber(m_Source == null ? null : m_Source.Number);
+			}
+			finally
+			{
+				m_RefreshSection.Leave();
+			}
+		}
+
+		#region Source Callbacks
+
+		/// <summary>
+		/// Subscribe to the source events.
+		/// </summary>
+		/// <param name="source"></param>
+		private void Subscribe(IConferenceSource source)
+		{
+			if (source == null)
+				return;
+
+			source.OnNameChanged += SourceOnNameChanged;
+			source.OnNumberChanged += SourceOnNumberChanged;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the source events.
+		/// </summary>
+		/// <param name="source"></param>
+		private void Unsubscribe(IConferenceSource source)
+		{
+			if (source == null)
+				return;
+
+			source.OnNameChanged -= SourceOnNameChanged;
+			source.OnNumberChanged -= SourceOnNumberChanged;
+		}
+
+		private void SourceOnNumberChanged(object sender, StringEventArgs stringEventArgs)
+		{
+			RefreshIfVisible();
+		}
+
+		private void SourceOnNameChanged(object sender, StringEventArgs stringEventArgs)
+		{
+			RefreshIfVisible();
+		}
+
+		#endregion
+
+		#region View Callbacks
+
+		protected override void Subscribe(IVtcReferencedHangupView view)
+		{
+			base.Subscribe(view);
+
+			view.OnHangupButtonPressed += ViewOnHangupButtonPressed;
+		}
+
+		protected override void Unsubscribe(IVtcReferencedHangupView view)
+		{
+			base.Unsubscribe(view);
+
+			view.OnHangupButtonPressed -= ViewOnHangupButtonPressed;
+		}
+
+		private void ViewOnHangupButtonPressed(object sender, EventArgs eventArgs)
+		{
+			if (m_Source != null)
+				m_Source.Hangup();
+		}
+
+		#endregion
 	}
 }
