d26b48f1d4fd8e59bb00d0676f2c97c7826a4096
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-24 17:02:10 -0500

    Begin implementing colour mode to font colour utils

--- ICD.Profound.ConnectPRO/Themes/UserInterface/IViews/Colors.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/IViews/Colors.cs
@@ -1,4 +1,6 @@
-﻿namespace ICD.Profound.ConnectPRO.Themes.UserInterface.IViews
+﻿using System;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.IViews
 {
 	public enum eDisplayColor
 	{
@@ -13,5 +15,56 @@
 		Grey,
 		Yellow,
 		Green
+	}
+
+	public static class Colors
+	{
+		private const string COLOR_WHITE = "#FFFFFF";
+		private const string COLOR_LIGHT_GREY = "#8B8B8B";
+		private const string COLOR_DARK_GREY = "#535353";
+
+		/// <summary>
+		/// Gets the hex color code for the display text in the current color mode.
+		/// </summary>
+		/// <param name="displayColor"></param>
+		/// <returns></returns>
+		public static string DisplayColorToTextColor(eDisplayColor displayColor)
+		{
+			switch (displayColor)
+			{
+				case eDisplayColor.Grey:
+					return COLOR_LIGHT_GREY;
+				case eDisplayColor.Yellow:
+					return COLOR_DARK_GREY;
+				case eDisplayColor.Green:
+					return COLOR_WHITE;
+
+				default:
+					throw new ArgumentOutOfRangeException("displayColor");
+			}
+		}
+
+		/// <summary>
+		/// Gets the hex color code for the source text in the current color mode.
+		/// </summary>
+		/// <param name="sourceColor"></param>
+		/// <returns></returns>
+		public static string SourceColorToTextColor(eSourceColor sourceColor)
+		{
+			switch (sourceColor)
+			{
+				case eSourceColor.White:
+					return COLOR_DARK_GREY;
+				case eSourceColor.Grey:
+					return COLOR_LIGHT_GREY;
+				case eSourceColor.Yellow:
+					return COLOR_DARK_GREY;
+				case eSourceColor.Green:
+					return COLOR_WHITE;
+
+				default:
+					throw new ArgumentOutOfRangeException("sourceColor");
+			}
+		}
 	}
 }
