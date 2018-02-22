127bb27ae92fa030b00f6a9ea13489a195cc880f
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-22 16:11:17 -0500

    Initial commit of CamerasView

---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Views/Cameras/CamerasView.cs
@@ -0,0 +1,18 @@
+﻿using ICD.Connect.Panels;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.Cameras;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Views.Cameras
+{
+	public sealed partial class CamerasView : AbstractView, ICamerasView
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="panel"></param>
+		/// <param name="theme"></param>
+		public CamerasView(ISigInputOutput panel, ConnectProTheme theme)
+			: base(panel, theme)
+		{
+		}
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Views/Cameras/CamerasView.generated.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using ICD.Connect.Panels;
+using ICD.Connect.UI.Controls;
+using ICD.Connect.UI.Controls.Buttons;
+using ICD.Connect.UI.Controls.Lists;
+using ICD.Connect.UI.Controls.Pages;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Views.Cameras
+{
+	public sealed partial class CamerasView
+	{
+		private VtProSubpage m_Subpage;
+		private VtProDynamicButtonList m_ButtonList;
+		private VtProButton m_CloseButton;
+
+		/// <summary>
+		/// Instantiates the view controls.
+		/// </summary>
+		/// <param name="panel"></param>
+		/// <param name="parent"></param>
+		/// <param name="index"></param>
+		protected override void InstantiateControls(ISigInputOutput panel, IVtProParent parent, ushort index)
+		{
+			m_Subpage = new VtProSubpage(panel, parent)
+			{
+				DigitalVisibilityJoin = 96
+			};
+
+			m_ButtonList = new VtProDynamicButtonList(4, panel as IPanelDevice, m_Subpage)
+			{
+				MaxSize = 8
+			};
+
+			m_CloseButton = new VtProButton(panel, m_Subpage)
+			{
+				DigitalPressJoin = 526
+			};
+		}
+
+		/// <summary>
+		/// Gets the child controls.
+		/// </summary>
+		/// <returns></returns>
+		protected override IEnumerable<IVtProControl> GetChildren()
+		{
+			yield return m_Subpage;
+			yield return m_ButtonList;
+			yield return m_CloseButton;
+		}
+	}
+}
