e1c1705809ae89a71f02cefdc1e71c13c1600ce8
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-27 16:32:12 -0500

    Initial commit of DisabledAlertPresenter

---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/IPresenters/IDisabledAlertPresenter.cs
@@ -0,0 +1,8 @@
+﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters
+{
+	public interface IDisabledAlertPresenter : IPresenter<IDisabledAlertView>
+	{
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/IViews/IDisabledAlertView.cs
@@ -0,0 +1,6 @@
+﻿namespace ICD.Profound.ConnectPRO.Themes.UserInterface.IViews
+{
+	public interface IDisabledAlertView : IView
+	{
+	}
+}
