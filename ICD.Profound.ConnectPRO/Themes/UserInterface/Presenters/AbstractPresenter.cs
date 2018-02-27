340f3157f61bc86e332a71ed8d5039d952cd0a3e
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-27 18:59:58 -0500

    Making UI synchronous

--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/AbstractPresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/AbstractPresenter.cs
@@ -228,7 +228,8 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters
 		[PublicAPI]
 		public void RefreshAsync()
 		{
-			m_AsyncRefreshHandle = ThreadingUtils.SafeInvoke(Refresh);
+			Refresh();
+			//m_AsyncRefreshHandle = ThreadingUtils.SafeInvoke(Refresh);
 		}
 
 		/// <summary>
