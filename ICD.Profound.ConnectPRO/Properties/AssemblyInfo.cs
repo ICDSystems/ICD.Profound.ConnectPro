9ba6efdd71520d08ab537a8d2b3fe9e95ad17fb9
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-24 13:52:31 -0400

    chore: Adding informational version

--- ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
+++ ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
@@ -5,3 +5,8 @@
 [assembly: AssemblyProduct("ICD.Profound.ConnectPRO")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("2.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.0")]
+#if SIMPLSHARP
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.0.0")]
+#endif
