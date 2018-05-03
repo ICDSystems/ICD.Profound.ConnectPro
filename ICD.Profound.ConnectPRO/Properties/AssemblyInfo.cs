ea3c2ad8c245cb561656947cf7c9b460d5fe9359
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-03 11:44:33 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
+++ ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Profound.ConnectPRO")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
 
 [assembly: AssemblyInformationalVersion("1.0.0")]
 #if SIMPLSHARP
