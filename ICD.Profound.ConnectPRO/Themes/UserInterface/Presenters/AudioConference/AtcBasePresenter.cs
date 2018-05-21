f8838a5c6710b1c038a9222b89b6f1ad79321bd7
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-20 21:56:24 -0400

    feat: Begin implementing AtcBasePresenter callbacks

--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/AudioConference/AtcBasePresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/AudioConference/AtcBasePresenter.cs
@@ -1,4 +1,5 @@
-﻿using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+﻿using ICD.Profound.ConnectPRO.Rooms;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.AudioConference;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.AudioConference;
@@ -17,5 +18,49 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.AudioConferenc
 			: base(nav, views, theme)
 		{
 		}
+
+		#region Room Callbacks
+
+		/// <summary>
+		/// Subscribe to the room events.
+		/// </summary>
+		/// <param name="room"></param>
+		protected override void Subscribe(IConnectProRoom room)
+		{
+			base.Subscribe(room);
+		}
+
+		/// <summary>
+		/// Unsubscribe from the room events.
+		/// </summary>
+		/// <param name="room"></param>
+		protected override void Unsubscribe(IConnectProRoom room)
+		{
+			base.Unsubscribe(room);
+		}
+
+		#endregion
+
+		#region View Callbacks
+
+		/// <summary>
+		/// Subscribe to the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Subscribe(IAtcBaseView view)
+		{
+			base.Subscribe(view);
+		}
+
+		/// <summary>
+		/// Unsubscribe from the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Unsubscribe(IAtcBaseView view)
+		{
+			base.Unsubscribe(view);
+		}
+
+		#endregion
 	}
 }
