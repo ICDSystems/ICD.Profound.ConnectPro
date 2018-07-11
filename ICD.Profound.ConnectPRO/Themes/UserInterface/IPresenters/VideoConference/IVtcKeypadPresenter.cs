1bcd1baf4288e33fbe9f9af53062eef755134ca4
Jeffery Thompson <jeffery.thompson@wedoresi.com>
2018-07-11 13:17:34 -0400

    feat: add keypad views and presenters

---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/IPresenters/VideoConference/IVtcKeypadPresenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.VideoConference
+{
+	public interface IVtcKeypadPresenter : IPresenter
+	{
+		event EventHandler OnKeyboardButtonPressed;
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/VideoConference/VtcKeypadPresenter.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.Conferencing.Controls.Dialing;
+using ICD.Connect.Conferencing.EventArguments;
+using ICD.Connect.UI.EventArguments;
+using ICD.Connect.UI.Utils;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters.VideoConference;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
+using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews.VideoConference;
+
+namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.VideoConference
+{
+	public class VtcKeypadPresenter : AbstractPresenter<IVtcKeypadView>, IVtcKeypadPresenter
+	{
+		public event EventHandler OnKeyboardButtonPressed; 
+
+		private readonly KeypadStringBuilder m_StringBuilder;
+
+		private bool m_RefreshTextField;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="nav"></param>
+		/// <param name="views"></param>
+		/// <param name="theme"></param>
+		public VtcKeypadPresenter(INavigationController nav, IViewFactory views, ConnectProTheme theme)
+			: base(nav, views, theme)
+		{
+			m_StringBuilder = new KeypadStringBuilder();
+			m_StringBuilder.OnStringChanged += StringBuilderOnStringChanged;
+
+			m_RefreshTextField = true;
+		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		public override void Dispose()
+		{
+			OnKeyboardButtonPressed = null;
+
+			base.Dispose();
+		}
+
+		/// <summary>
+		/// Updates the view.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Refresh(IVtcKeypadView view)
+		{
+			base.Refresh(view);
+
+			if (m_RefreshTextField)
+				view.SetText(m_StringBuilder.ToString());
+		}
+
+		/// <summary>
+		/// Called when the stringbuilder string changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="stringEventArgs"></param>
+		private void StringBuilderOnStringChanged(object sender, StringEventArgs stringEventArgs)
+		{
+			// Refresh synchronously to avoid interfering with user input.
+			RefreshIfVisible(false);
+		}
+
+		#region View Callbacks
+
+		/// <summary>
+		/// Subscribe to the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Subscribe(IVtcKeypadView view)
+		{
+			base.Subscribe(view);
+
+			view.OnBackspaceButtonPressed += ViewOnBackspaceButtonPressed;
+			view.OnTextEntered += ViewOnTextEntered;
+			view.OnKeyPressed += ViewOnKeyPressed;
+			view.OnDialButtonPressed += ViewOnDialButtonPressed;
+			view.OnKeyboardButtonPressed += ViewOnKeyboardButtonPressed;
+		}
+
+		/// <summary>
+		/// Unsubscribe from the view events.
+		/// </summary>
+		/// <param name="view"></param>
+		protected override void Unsubscribe(IVtcKeypadView view)
+		{
+			base.Unsubscribe(view);
+
+			view.OnBackspaceButtonPressed -= ViewOnBackspaceButtonPressed;
+			view.OnTextEntered -= ViewOnTextEntered;
+			view.OnKeyPressed -= ViewOnKeyPressed;
+		}
+
+		/// <summary>
+		/// Called when the user enters text directly in the text field.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="stringEventArgs"></param>
+		private void ViewOnTextEntered(object sender, StringEventArgs stringEventArgs)
+		{
+			m_RefreshTextField = false;
+			m_StringBuilder.SetString(stringEventArgs.Data);
+			m_RefreshTextField = true;
+		}
+
+		/// <summary>
+		/// Called when the user presses a key.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="eventArgs"></param>
+		private void ViewOnKeyPressed(object sender, KeyboardKeyEventArgs eventArgs)
+		{
+			char character = eventArgs.Data.GetChar(false, false);
+
+			m_StringBuilder.AppendCharacter(character);
+		}
+
+		/// <summary>
+		/// Called when the user presses the submit button.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="eventArgs"></param>
+		private void ViewOnDialButtonPressed(object sender, EventArgs eventArgs)
+		{
+			string number = m_StringBuilder.ToString();
+
+			IDialingDeviceControl control = Room == null ? null : Room.ConferenceManager.GetDialingProvider(eConferenceSourceType.Video);
+			if (control != null)
+				control.Dial(number);
+
+			ShowView(false);
+		}
+
+		private void ViewOnKeyboardButtonPressed(object sender, EventArgs eventArgs)
+		{
+			OnKeyboardButtonPressed.Raise(this);
+		}
+
+		/// <summary>
+		/// Called when the iser presses the backspace button.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="eventArgs"></param>
+		private void ViewOnBackspaceButtonPressed(object sender, EventArgs eventArgs)
+		{
+			m_StringBuilder.Backspace();
+		}
+
+		/// <summary>
+		/// Called when the view visibility changes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		protected override void ViewOnVisibilityChanged(object sender, BoolEventArgs args)
+		{
+			base.ViewOnVisibilityChanged(sender, args);
+
+			m_StringBuilder.Clear();
+		}
+
+		#endregion
+	}
+}
