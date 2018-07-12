398ead5558ee5c03a39c4e534d287bcb54cbe60e
Chris Cameron <chris.cameron@wedoresi.com>
2018-07-12 16:59:19 -0400

    refactor: Removing unused code

--- ICD.Profound.ConnectPRO/Routing/ConnectProRouting.cs
+++ ICD.Profound.ConnectPRO/Routing/ConnectProRouting.cs
@@ -719,19 +719,6 @@ namespace ICD.Profound.ConnectPRO.Routing
 		}
 
 		/// <summary>
-		/// Unroutes the given video source from all display destinations.
-		/// </summary>
-		/// <param name="source"></param>
-		public void UnrouteVideo(ISource source)
-		{
-			if (source == null)
-				throw new ArgumentNullException("source");
-
-			foreach (IDestination destination in GetDisplayDestinations())
-				RoutingGraph.Unroute(source, destination, eConnectionType.Video, m_Room.Id);
-		}
-
-		/// <summary>
 		/// Fully unroutes the VTC from all displays.
 		/// </summary>
 		public void UnrouteVtc()
@@ -742,20 +729,6 @@ namespace ICD.Profound.ConnectPRO.Routing
 
 			IRouteSourceControl sourceControl = codec.Controls.GetControl<IRouteSourceControl>();
 			RoutingGraph.Unroute(sourceControl, EnumUtils.GetFlagsAllValue<eConnectionType>(), m_Room.Id);
-		}
-
-		/// <summary>
-		/// Unroute all sources from all destinations.
-		/// </summary>
-		public void UnrouteAll()
-		{
-			UnrouteVtc();
-
-			foreach (IDestination display in GetDisplayDestinations())
-				RoutingGraph.Unroute(display, EnumUtils.GetFlagsAllValue<eConnectionType>(), m_Room.Id);
-
-			foreach (IDestination audio in GetAudioDestinations())
-				RoutingGraph.Unroute(audio, EnumUtils.GetFlagsAllValue<eConnectionType>(), m_Room.Id);
 		}
 
 		/// <summary>
