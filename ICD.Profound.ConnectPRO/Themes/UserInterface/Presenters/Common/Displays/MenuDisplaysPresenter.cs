010ba4c0089f7e6fd9d856c3bf56229cc3ff6807
Chris Cameron <chris.cameron@wedoresi.com>
2018-08-08 17:04:56 -0400

    perf: Caching displays presenter state

--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Displays/MenuDisplaysPresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Displays/MenuDisplaysPresenter.cs
@@ -18,8 +18,6 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Display
 {
 	public sealed class MenuDisplaysPresenter : AbstractPresenter<IMenuDisplaysView>, IMenuDisplaysPresenter
 	{
-		private const int MAX_LINE_WIDTH = 20;
-
 		public event MenuDestinationPressedCallback OnDestinationPressed;
 
 		private readonly SafeCriticalSection m_RefreshSection;
@@ -42,7 +40,13 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Display
 
 				m_ActiveSource = value;
 
-				RefreshIfVisible();
+				bool refresh = false;
+
+				refresh |= m_Display1.SetActiveSource(m_ActiveSource);
+				refresh |= m_Display2.SetActiveSource(m_ActiveSource);
+
+				if (refresh)
+					RefreshIfVisible();
 			}
 		}
 
@@ -58,20 +62,7 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Display
 			m_RefreshSection = new SafeCriticalSection();
 
 			m_Display1 = new MenuDisplaysPresenterDisplay();
-			m_Display1.OnPropertyChange += Display1OnPropertyChange;
-
 			m_Display2 = new MenuDisplaysPresenterDisplay();
-			m_Display2.OnPropertyChange += Display2OnPropertyChange;
-		}
-
-		private void Display1OnPropertyChange(object sender, EventArgs eventArgs)
-		{
-			RefreshIfVisible();
-		}
-
-		private void Display2OnPropertyChange(object sender, EventArgs eventArgs)
-		{
-			RefreshIfVisible();
 		}
 
 		/// <summary>
@@ -90,12 +81,22 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Display
 		/// <param name="room"></param>
 		public override void SetRoom(IConnectProRoom room)
 		{
-			base.SetRoom(room);
-
 			bool combine = room != null && room.IsCombineRoom();
 
-			m_Display1.RoomCombine = combine;
-			m_Display2.RoomCombine = combine;
+			m_Display1.SetRoomCombine(combine);
+			m_Display2.SetRoomCombine(combine);
+
+			IDestination[] destinations = room == null ? new IDestination[0] : room.Routing.GetDisplayDestinations().ToArray();
+
+			IDestination dest1;
+			destinations.TryElementAt(0, out dest1);
+			m_Display1.SetDestination(dest1);
+
+			IDestination dest2;
+			destinations.TryElementAt(1, out dest2);
+			m_Display2.SetDestination(dest2);
+
+			base.SetRoom(room);
 		}
 
 		protected override void Refresh(IMenuDisplaysView view)
@@ -106,81 +107,27 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Display
 
 			try
 			{
-				/*
-				bool combine = Room != null && Room.IsCombineRoom();
+				/*IcdStopwatch.Profile(() =>
+				                     {
+									 */
+					                     // Display 1
+					                     view.SetDisplay1Color(m_Display1.Color);
+					                     view.SetDisplay1SourceText(m_Display1.SourceName);
+					                     view.SetDisplay1Line1Text(m_Display1.Line1);
+					                     view.SetDisplay1Line2Text(m_Display1.Line2);
+					                     view.SetDisplay1Icon(m_Display1.Icon);
+					                     view.ShowDisplay1SpeakerButton(m_Display1.ShowSpeaker);
+					                     view.SetDisplay1SpeakerButtonActive(m_Display1.AudioActive);
 
-				string display1DestinationName =
-					Display1Destination == null
-						? string.Empty
-						: Display1Destination.GetName(combine) ?? string.Empty;
-
-				bool display1HasControl = HasDeviceControl(m_Display1RoutedSource);
-
-				eDisplayColor display1Color = m_ActiveSource == null || m_ActiveSource == m_Display1RoutedSource
-					                              ? m_Display1RoutedSource == null
-						                                ? eDisplayColor.Grey
-						                                : display1HasControl
-							                                  ? eDisplayColor.Green
-							                                  : eDisplayColor.White
-					                              : eDisplayColor.Yellow;
-
-				string display1Text = m_ActiveSource == null || m_ActiveSource == m_Display1RoutedSource
-					                      ? display1DestinationName
-					                      : string.Format("PRESS TO SHOW SELECTION ON {0}", display1DestinationName);
-				display1Text = display1Text.ToUpper();
-
-				string display1SourceName = m_Display1RoutedSource == null
-					                            ? string.Empty
-					                            : m_Display1RoutedSource.GetNameOrDeviceName(combine);
-
-				string display1Line1;
-				string display1Line2;
-
-				if (display1Text.Length <= MAX_LINE_WIDTH)
-				{
-					display1Line1 = display1Text;
-					display1Line2 = string.Empty;
-				}
-				else
-				{
-					// Find the space closest to the middle of the text and split.
-					int middleIndex = display1Text.Length / 2;
-					int splitIndex = display1Text.FindIndices(char.IsWhiteSpace).GetClosest(i => i - middleIndex);
-
-					display1Line1 = display1Text.Substring(0, splitIndex).Trim();
-					display1Line2 = display1Text.Substring(splitIndex + 1).Trim();
-				}
-
-				if (display1HasControl)
-				{
-					display1Line1 = "PRESS FOR CONTROLS";
-					display1Line2 = string.Empty;
-				}
-
-				// Text
-				string display1HexColor = Colors.DisplayColorToTextColor(display1Color);
-				display1SourceName = HtmlUtils.FormatColoredText(display1SourceName, display1HexColor);
-				display1Line1 = HtmlUtils.FormatColoredText(display1Line1, display1HexColor);
-				display1Line2 = HtmlUtils.FormatColoredText(display1Line2, display1HexColor);
-				 */
-
-				// Display 1
-				view.SetDisplay1Color(m_Display1.Color);
-				view.SetDisplay1SourceText(m_Display1.SourceName);
-				view.SetDisplay1Line1Text(m_Display1.Line1);
-				view.SetDisplay1Line2Text(m_Display1.Line2);
-				view.SetDisplay1Icon(m_Display1.Icon);
-				view.ShowDisplay1SpeakerButton(m_Display1.ShowSpeaker);
-				view.SetDisplay1SpeakerButtonActive(m_Display1.AudioActive);
-
-				// Display 2
-				view.SetDisplay2Color(m_Display2.Color);
-				view.SetDisplay2SourceText(m_Display2.SourceName);
-				view.SetDisplay2Line2Text(m_Display2.Line1);
-				view.SetDisplay2Line2Text(m_Display2.Line2);
-				view.SetDisplay2Icon(m_Display2.Icon);
-				view.ShowDisplay2SpeakerButton(m_Display2.ShowSpeaker);
-				view.SetDisplay2SpeakerButtonActive(m_Display2.AudioActive);
+					                     // Display 2
+					                     view.SetDisplay2Color(m_Display2.Color);
+					                     view.SetDisplay2SourceText(m_Display2.SourceName);
+					                     view.SetDisplay2Line1Text(m_Display2.Line1);
+					                     view.SetDisplay2Line2Text(m_Display2.Line2);
+					                     view.SetDisplay2Icon(m_Display2.Icon);
+					                     view.ShowDisplay2SpeakerButton(m_Display2.ShowSpeaker);
+					                     view.SetDisplay2SpeakerButtonActive(m_Display2.AudioActive);
+									// }, string.Format("{0} refresh displays", GetType().Name));
 			}
 			finally
 			{
@@ -190,14 +137,19 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Display
 
 		public void SetRouting(Dictionary<IDestination, IcdHashSet<ISource>> routing, IcdHashSet<ISource> activeAudio)
 		{
-			m_Display1.RoutedSource = GetRoutedSource(m_Display1.Destination, routing);
-			m_Display1.AudioActive = m_Display1.RoutedSource != null && activeAudio.Contains(m_Display1.RoutedSource);
+			bool refresh = false;
 
-			m_Display2.RoutedSource = GetRoutedSource(m_Display2.Destination, routing);
-			m_Display2.AudioActive = m_Display2.RoutedSource != null && activeAudio.Contains(m_Display2.RoutedSource);
+			refresh |= m_Display1.SetRoutedSource(GetRoutedSource(m_Display1.Destination, routing));
+			refresh |= m_Display1.SetAudioActive(m_Display1.RoutedSource != null && activeAudio.Contains(m_Display1.RoutedSource));
+
+			refresh |= m_Display2.SetRoutedSource(GetRoutedSource(m_Display2.Destination, routing));
+			refresh |= m_Display2.SetAudioActive(m_Display2.RoutedSource != null && activeAudio.Contains(m_Display2.RoutedSource));
+
+			if (refresh)
+				RefreshIfVisible();
 		}
 
-		private ISource GetRoutedSource(IDestination destination, Dictionary<IDestination, IcdHashSet<ISource>> routing)
+		private static ISource GetRoutedSource(IDestination destination, IDictionary<IDestination, IcdHashSet<ISource>> routing)
 		{
 			if (destination == null)
 				return null;
--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Displays/MenuDisplaysPresenterDisplay.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/Common/Displays/MenuDisplaysPresenterDisplay.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using ICD.Common.Utils.Extensions;
+﻿using ICD.Common.Utils.Extensions;
 using ICD.Connect.Routing.Connections;
+using ICD.Connect.Routing.Endpoints;
 using ICD.Connect.Routing.Endpoints.Destinations;
 using ICD.Connect.Routing.Endpoints.Sources;
+using ICD.Connect.Settings;
+using ICD.Connect.UI.Utils;
 using ICD.Profound.ConnectPRO.Routing;
 using ICD.Profound.ConnectPRO.Routing.Endpoints.Sources;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
@@ -11,87 +13,209 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters.Common.Display
 {
 	public sealed class MenuDisplaysPresenterDisplay
 	{
-		public event EventHandler OnPropertyChange;
+		private const int MAX_LINE_WIDTH = 20;
 
 		private eDisplayColor m_Color;
 		private string m_SourceName;
 		private string m_Line1;
 		private string m_Line2;
-		private string m_Icon;
-		private bool m_ShowSpeaker;
 		private bool m_AudioActive;
 		private ISource m_RoutedSource;
+		private ISource m_ActiveSource;
+		private IDestination m_Destination;
+		private bool m_RoomCombine;
+		private bool m_HasControl;
+		private bool m_ShowSpeaker;
+		private string m_Icon;
+
+		#region Properties
 
 		public eDisplayColor Color { get { return m_Color; } }
 		public string SourceName { get { return m_SourceName; } }
 		public string Line1 { get { return m_Line1; } }
 		public string Line2 { get { return m_Line2; } }
 
-		public string Icon { get; private set; }
+		public string Icon { get { return m_Icon; } }
 
-		public bool ShowSpeaker
+		public bool ShowSpeaker { get { return m_ShowSpeaker; } }
+
+		public bool AudioActive { get { return m_AudioActive; } }
+
+		public IDestination Destination { get { return m_Destination; } }
+
+		public ISource RoutedSource { get { return m_RoutedSource; } }
+
+		#endregion
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public MenuDisplaysPresenterDisplay()
 		{
-			get
-			{
-				return (m_ActiveSource == null || m_ActiveSource == m_RoutedSource) &&
-					   m_RoutedSource != null &&
-					   m_RoutedSource.ConnectionType.HasFlag(eConnectionType.Audio);
-			}
+			m_Color = eDisplayColor.Grey;
 		}
 
-		public bool AudioActive
+		#region Methods
+
+		public bool SetRoutedSource(ISource routedSource)
 		{
-			get
-			{
-				return m_AudioActive;
-			}
-			set
-			{
-				if (value == m_AudioActive)
-					return;
+			if (routedSource == m_RoutedSource)
+				return false;
 
-				m_AudioActive = value;
+			m_RoutedSource = routedSource;
 
-				OnPropertyChange.Raise(this);
-			}
+			// Update has control
+			UpdateHasControl();
+
+			// Update the color
+			UpdateColor();
+
+			// Update icon
+			UpdateIcon();
+
+			// Update the labels
+			UpdateLabels();
+
+			// Update show speaker
+			UpdateShowSpeaker();
+
+			return true;
 		}
 
-		public bool HasControl { get { return HasDeviceControl(m_RoutedSource); } }
-
-		public IDestination Destination { get; set; }
-
-		public ISource RoutedSource
+		public bool SetActiveSource(ISource activeSource)
 		{
-			get { return m_RoutedSource; }
-			set
-			{
-				if (value == m_RoutedSource)
-					return;
+			if (activeSource == m_ActiveSource)
+				return false;
 
-				m_RoutedSource = value;
+			m_ActiveSource = activeSource;
 
-				// Update icon
-				ConnectProSource source = RoutedSource as ConnectProSource;
-				string display1Icon = source == null ? null : source.Icon;
-				Icon = Icons.GetDisplayIcon(display1Icon, Color);
+			// Update the color
+			UpdateColor();
 
-				// Update control state
-				HasControl = m_RoutedSource != null && ConnectProRouting.CanControl(m_RoutedSource);
+			// Update icon
+			UpdateIcon();
 
-				// Show speaker
-				return (m_ActiveSource == null || m_ActiveSource == m_RoutedSource) &&
-					   m_RoutedSource != null &&
-					   m_RoutedSource.ConnectionType.HasFlag(eConnectionType.Audio);
+			// Update the labels
+			UpdateLabels();
 
-				OnPropertyChange.Raise(this);
-			}
+			// Update show speaker
+			UpdateShowSpeaker();
+
+			return true;
 		}
 
-		public string Name { get; }
-
-		public bool RoomCombine
+		public bool SetAudioActive(bool value)
 		{
-			get; set;
+			if (value == m_AudioActive)
+				return false;
+
+			m_AudioActive = value;
+
+			return true;
+		}
+
+		public bool SetDestination(IDestination destination)
+		{
+			if (destination == m_Destination)
+				return false;
+
+			m_Destination = destination;
+
+			// Update the labels
+			UpdateLabels();
+
+			return true;
+		}
+
+		public bool SetRoomCombine(bool combine)
+		{
+			if (combine == m_RoomCombine)
+				return false;
+
+			m_RoomCombine = true;
+
+			// Update the labels
+			UpdateLabels();
+
+			return true;
+		}
+
+		#endregion
+
+		private void UpdateHasControl()
+		{
+			m_HasControl = m_RoutedSource != null && ConnectProRouting.CanControl(m_RoutedSource);
+		}
+
+		private void UpdateColor()
+		{
+			m_Color = m_ActiveSource == null || m_ActiveSource == m_RoutedSource
+				          ? m_RoutedSource == null
+					            ? eDisplayColor.Grey
+					            : m_HasControl
+						              ? eDisplayColor.Green
+						              : eDisplayColor.White
+				          : eDisplayColor.Yellow;
+		}
+
+		private void UpdateIcon()
+		{
+
+			ConnectProSource source = m_RoutedSource as ConnectProSource;
+			string icon = source == null ? null : source.Icon;
+			m_Icon = Icons.GetDisplayIcon(icon, m_Color);
+		}
+
+		private void UpdateLabels()
+		{
+			if (m_HasControl)
+			{
+				m_Line1 = "PRESS FOR CONTROLS";
+				m_Line2 = string.Empty;
+			}
+			else
+			{
+				string destinationName =
+					m_Destination == null
+						? string.Empty
+						: m_Destination.GetName(m_RoomCombine) ?? string.Empty;
+
+				string text = m_ActiveSource == null || m_ActiveSource == m_RoutedSource
+					              ? destinationName
+					              : string.Format("PRESS TO SHOW SELECTION ON {0}", destinationName);
+				text = text.ToUpper();
+
+				if (text.Length <= MAX_LINE_WIDTH)
+				{
+					m_Line1 = text;
+					m_Line2 = string.Empty;
+				}
+				else
+				{
+					// Find the space closest to the middle of the text and split.
+					int middleIndex = text.Length / 2;
+					int splitIndex = text.FindIndices(char.IsWhiteSpace).GetClosest(i => i - middleIndex);
+
+					m_Line1 = text.Substring(0, splitIndex).Trim();
+					m_Line2 = text.Substring(splitIndex + 1).Trim();
+				}
+			}
+
+			m_SourceName = m_RoutedSource == null
+				               ? string.Empty
+				               : m_RoutedSource.GetNameOrDeviceName(m_RoomCombine);
+
+			string display1HexColor = Colors.DisplayColorToTextColor(m_Color);
+			m_SourceName = HtmlUtils.FormatColoredText(m_SourceName, display1HexColor);
+			m_Line1 = HtmlUtils.FormatColoredText(m_Line1, display1HexColor);
+			m_Line2 = HtmlUtils.FormatColoredText(m_Line2, display1HexColor);
+		}
+
+		private void UpdateShowSpeaker()
+		{
+			m_ShowSpeaker = (m_ActiveSource == null || m_ActiveSource == m_RoutedSource) &&
+			                m_RoutedSource != null &&
+			                m_RoutedSource.ConnectionType.HasFlag(eConnectionType.Audio);
 		}
 	}
 }
