d604c0347a6721aee076491585fda6d8ee2013d5
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-07 15:33:02 -0500

    chore: Updating AssemblyInfo

--- ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
+++ ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Profound.ConnectPRO")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Profound.ConnectPRO")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("8.0.0.0")]
 
 [assembly: AssemblyInformationalVersion("1.2.0")]
