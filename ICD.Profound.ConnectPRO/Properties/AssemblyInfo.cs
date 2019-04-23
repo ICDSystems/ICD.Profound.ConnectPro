cc9a3667f59721076f281943d7b92777c54b14d2
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-23 11:55:10 -0400

    chore: Incrementing informational version

--- ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
+++ ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("8.1.0.0")]
 
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
 #if SIMPLSHARP
-[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.2.0")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.3.0")]
 #endif
