3569a868e9dfb86cca3ad8dd8df86047c9007b6b
Jeffery Thompson <jeffery.thompson@wedoresi.com>
2019-05-28 14:40:03 -0400

    feat: IMaskedSourceInfo and implementations

---
+++ ICD.Profound.ConnectPRO/Routing/Masking/AbstractMaskedSourceInfo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ICD.Connect.Routing.Endpoints.Destinations;
+using ICD.Connect.Routing.Endpoints.Sources;
+
+namespace ICD.Profound.ConnectPRO.Routing.Masking
+{
+	public abstract class AbstractMaskedSourceInfo : IMaskedSourceInfo
+	{
+		private ISource m_Source;
+		private bool m_Mask;
+		private bool? m_MaskOverride;
+
+		#region Properties
+
+		public ISource Source
+		{
+			get { return m_Source; }
+			set
+			{
+				if (m_Source == value)
+					return;
+				Unsubscribe(m_Source);
+				m_Source = value;
+				Subscribe(m_Source);
+			}
+		}
+
+		public bool Mask 
+		{ 
+			get { return m_Mask; }
+			protected set
+			{
+				if (m_Mask == value)
+					return;
+
+				m_Mask = value;
+				CheckMaskState();
+			}
+		}
+
+		public bool? MaskOverride
+		{
+			get { return m_MaskOverride; }
+			set
+			{
+				if (m_MaskOverride == value)
+					return;
+
+				m_MaskOverride = value;
+				CheckMaskState();
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		protected abstract void PerformMask();
+
+		protected abstract void PerformUnmask();
+
+		protected virtual void Subscribe(ISource source)
+		{
+		}
+
+		protected virtual void Unsubscribe(ISource source)
+		{
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void CheckMaskState()
+		{
+			if (MaskOverride ?? Mask)
+				PerformMask();
+			else
+				PerformUnmask();
+		}
+
+		#endregion
+
+		public void Dispose()
+		{
+			DisposeFinal();
+		}
+
+		protected virtual void DisposeFinal()
+		{
+			Source = null;
+		}
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Routing/Masking/ConferenceDeviceMaskedSourceInfo.cs
@@ -0,0 +1,170 @@
+﻿using System.Linq;
+using ICD.Connect.Conferencing.Conferences;
+using ICD.Connect.Conferencing.Controls.Dialing;
+using ICD.Connect.Conferencing.EventArguments;
+using ICD.Connect.Devices;
+using ICD.Connect.Routing.Endpoints.Sources;
+using ICD.Profound.ConnectPRO.Rooms;
+
+namespace ICD.Profound.ConnectPRO.Routing.Masking
+{
+	public sealed class ConferenceDeviceMaskedSourceInfo : AbstractMaskedSourceInfo
+	{
+		private IConferenceDeviceControl m_ConferenceControl;
+		private IConnectProRoom m_Room;
+
+		#region Properties
+
+		public IConnectProRoom Room
+		{
+			get { return m_Room; }
+			private set
+			{
+				if (m_Room == value)
+					return;
+				m_Room = value;
+				UpdateControl();
+			}
+		}
+
+		private IConferenceDeviceControl ConferenceControl
+		{
+			get { return m_ConferenceControl; }
+			set
+			{
+				if (m_ConferenceControl == value)
+					return;
+
+				Unsubscribe(m_ConferenceControl);
+				m_ConferenceControl = value;
+				Subscribe(m_ConferenceControl);
+			}
+		}
+
+		#endregion
+
+		public ConferenceDeviceMaskedSourceInfo(ISource source, IConnectProRoom room)
+		{
+			Room = room;
+			Source = source;
+		}
+
+		protected override void PerformMask()
+		{
+			Room.Routing.RouteOsd(this);
+		}
+
+		protected override void PerformUnmask()
+		{
+			Room.Routing.RouteVtc(Source, this);
+		}
+
+		private void UpdateMask()
+		{
+			if (m_ConferenceControl == null)
+				return;
+
+			Mask = ConferenceControl.GetConferences().All(c => c.Status != eConferenceStatus.Connected);
+		}
+
+		private void UpdateControl()
+		{
+			if (Source == null || Room == null)
+			{
+				ConferenceControl = null;
+				return;
+			}
+
+			var device = Room.Core.Originators.GetChild(Source.Device) as IDevice;
+			ConferenceControl = device == null ? null : device.Controls.GetControl<IConferenceDeviceControl>();
+			UpdateMask();
+		}
+
+		#region Source Callbacks 
+
+		protected override void Subscribe(ISource source)
+		{
+			base.Subscribe(source);
+
+			if (source == null)
+				return;
+
+			UpdateControl();
+		}
+
+		protected override void Unsubscribe(ISource source)
+		{
+			base.Unsubscribe(source);
+			
+			if (source == null || Room == null)
+				return;
+
+			ConferenceControl = null;
+		}
+
+		#endregion
+
+		#region Control Callbacks
+
+		private void Subscribe(IConferenceDeviceControl control)
+		{
+			if (control == null)
+				return;
+
+			control.OnConferenceAdded += ControlOnConferenceAdded;
+			control.OnConferenceRemoved += ControlOnConferenceRemoved;
+
+			foreach (var conference in control.GetConferences())
+				Subscribe(conference);
+		}
+
+		private void Unsubscribe(IConferenceDeviceControl control)
+		{
+			if (control == null)
+				return;
+
+			control.OnConferenceAdded -= ControlOnConferenceAdded;
+			control.OnConferenceRemoved -= ControlOnConferenceRemoved;
+
+			foreach (var conference in control.GetConferences())
+				Unsubscribe(conference);
+		}
+
+		private void ControlOnConferenceAdded(object sender, ConferenceEventArgs e)
+		{
+			Subscribe(e.Data);
+		}
+
+		private void ControlOnConferenceRemoved(object sender, ConferenceEventArgs e)
+		{
+			Unsubscribe(e.Data);
+		}
+
+		#endregion
+
+		#region Conference Callbacks
+
+		private void Subscribe(IConference conference)
+		{
+			if (conference == null)
+				return;
+
+			conference.OnStatusChanged += ConferenceOnStatusChanged;
+		}
+
+		private void Unsubscribe(IConference conference)
+		{
+			if (conference == null)
+				return;
+
+			conference.OnStatusChanged -= ConferenceOnStatusChanged;
+		}
+
+		private void ConferenceOnStatusChanged(object sender, ConferenceStatusEventArgs e)
+		{
+			UpdateMask();
+		}
+
+		#endregion
+	}
+}
