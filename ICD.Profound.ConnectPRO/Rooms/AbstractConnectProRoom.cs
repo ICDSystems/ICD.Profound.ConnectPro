db113848134ad82111c281cec44cb4c7b5473c30
Austin Noska <austin.noska@profound-tech.com>
2019-07-01 15:24:53 -0400

    chore: removing unused using directives

--- ICD.Profound.ConnectPRO/Rooms/AbstractConnectProRoom.cs
+++ ICD.Profound.ConnectPRO/Rooms/AbstractConnectProRoom.cs
@@ -17,7 +17,6 @@ using ICD.Connect.Calendaring.CalendarControl;
 using ICD.Connect.Conferencing.ConferenceManagers;
 using ICD.Connect.Conferencing.Conferences;
 using ICD.Connect.Conferencing.Controls.Dialing;
-using ICD.Connect.Conferencing.Devices;
 using ICD.Connect.Conferencing.DialContexts;
 using ICD.Connect.Conferencing.EventArguments;
 using ICD.Connect.Devices;
