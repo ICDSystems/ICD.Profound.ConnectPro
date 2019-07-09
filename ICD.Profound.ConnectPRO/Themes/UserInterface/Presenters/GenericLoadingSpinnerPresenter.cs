8492e671f83fdf10867b035ee4c805b37abe0b26
Austin Noska <austin.noska@profound-tech.com>
2019-07-09 10:56:33 -0400

    feat: Added showview overload to take a timeout parameter, and added timeout methods with a safetimer to hide the view after a specified amount of time.

--- ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/GenericLoadingSpinnerPresenter.cs
+++ ICD.Profound.ConnectPRO/Themes/UserInterface/Presenters/GenericLoadingSpinnerPresenter.cs
@@ -1,5 +1,8 @@
 ﻿using ICD.Common.Utils;
+using ICD.Common.Utils.EventArguments;
+using ICD.Common.Utils.Timers;
 using ICD.Connect.UI.Attributes;
+using ICD.Connect.UI.Mvp.Presenters;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IPresenters;
 using ICD.Profound.ConnectPRO.Themes.UserInterface.IViews;
 
@@ -9,6 +12,7 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters
 	public sealed class GenericLoadingSpinnerPresenter : AbstractUiPresenter<IGenericLoadingSpinnerView>, IGenericLoadingSpinnerPresenter
 	{
 		private readonly SafeCriticalSection m_RefreshSection;
+		private readonly SafeTimer m_LoadingTimeOut;
 
 		private string LoadingText { get; set; }
 
@@ -16,6 +20,7 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters
 			: base(nav, views, theme)
 		{
 			m_RefreshSection = new SafeCriticalSection();
+			m_LoadingTimeOut = SafeTimer.Stopped(TimeOut);
 		}
 
 		protected override void Refresh(IGenericLoadingSpinnerView view)
@@ -23,11 +28,57 @@ namespace ICD.Profound.ConnectPRO.Themes.UserInterface.Presenters
 			m_RefreshSection.Execute(() => view.SetText(LoadingText));
 		}
 
+		/// <summary>
+		/// Shows the view with the text parameter displayed.
+		/// </summary>
+		/// <param name="text"></param>
 		public void ShowView(string text)
 		{
 			LoadingText = text;
 			ShowView(true);
 			RefreshIfVisible();
+		}
+
+		/// <summary>
+		/// Shows the view with the text parameter displayed, and times out after timeoutMilliseconds.
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="timeoutMilliseconds"></param>
+		public void ShowView(string text, long timeoutMilliseconds)
+		{
+			ShowView(text);
+
+			m_LoadingTimeOut.Reset(timeoutMilliseconds);
+		}
+
+		/// <summary>
+		/// Time out the spinner with a generic message if the task takes too long.
+		/// </summary>
+		public void TimeOut()
+		{
+			TimeOut("Operation failed to complete.");
+		}
+
+		/// <summary>
+		/// Time out the spinner with a specific message if the task takes too long.
+		/// </summary>
+		/// <param name="text"></param>
+		public void TimeOut(string text)
+		{
+			ShowView(false);
+			Navigation.LazyLoadPresenter<IGenericAlertPresenter>().Show(text);
+		}
+
+		/// <summary>
+		/// When the view visibility changes stop the timeout timer.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		protected override void ViewOnVisibilityChanged(object sender, BoolEventArgs args)
+		{
+			base.ViewOnVisibilityChanged(sender, args);
+
+			m_LoadingTimeOut.Stop();
 		}
 	}
 }
