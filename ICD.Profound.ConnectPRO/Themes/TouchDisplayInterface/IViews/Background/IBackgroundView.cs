2f6dd8ce141fd5dd3e24b3e35d73a0685c73cd40
Jeffery Thompson <jeffery.thompson@wedoresi.com>
2019-09-05 10:24:31 -0400

    feat: touch display background views/presenters

---
+++ ICD.Profound.ConnectPRO/Themes/TouchDisplayInterface/IViews/Background/IBackgroundView.cs
@@ -0,0 +1,7 @@
+﻿namespace ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.IViews.Background
+{
+	public interface IBackgroundView : ITouchDisplayView
+	{
+		void SetBackgroundMode(eCueBackgroundMode mode);
+	}
+}
---
+++ ICD.Profound.ConnectPRO/Themes/TouchDisplayInterface/Presenters/Background/BackgroundPresenter.cs
@@ -0,0 +1,64 @@
+﻿using ICD.Common.Utils;
+using ICD.Connect.UI.Attributes;
+using ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.IPresenters.Background;
+using ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.IViews.Background;
+using ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.IPresenters;
+using ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.IViews;
+using System;
+
+namespace ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.Presenters.Background
+{
+	[PresenterBinding(typeof(ITouchDisplayBackgroundPresenter))]
+	public sealed class BackgroundPresenter : AbstractTouchDisplayPresenter<ITouchDisplayBackgroundView>, ITouchDisplayBackgroundPresenter
+	{
+		private readonly SafeCriticalSection m_RefreshSection;
+
+		public BackgroundPresenter(ITouchDisplayNavigationController nav, ITouchDisplayViewFactory views, ConnectProTheme theme) : base(nav, views, theme)
+		{
+			m_RefreshSection = new SafeCriticalSection();
+
+			Subscribe(theme);
+		}
+
+		public override void Dispose()
+		{
+			base.Dispose();
+
+			Unsubscribe(Theme);
+		}
+
+		protected override void Refresh(ITouchDisplayBackgroundView view)
+		{
+			base.Refresh(view);
+			
+			m_RefreshSection.Enter();
+			try
+			{
+				view.SetBackgroundMode(Theme.CueBackground);
+			}
+			finally
+			{
+				m_RefreshSection.Leave();
+			}
+		}
+
+		#region Theme Callbacks
+
+		private void Subscribe(ConnectProTheme theme)
+		{
+			theme.OnCueBackgroundChanged += ThemeOnCueBackgroundChanged;
+		}
+
+		private void Unsubscribe(ConnectProTheme theme)
+		{
+			theme.OnCueBackgroundChanged -= ThemeOnCueBackgroundChanged;
+		}
+
+		private void ThemeOnCueBackgroundChanged(object sender, EventArgs e)
+		{
+			RefreshIfVisible();
+		}
+
+		#endregion
+	}
+}
