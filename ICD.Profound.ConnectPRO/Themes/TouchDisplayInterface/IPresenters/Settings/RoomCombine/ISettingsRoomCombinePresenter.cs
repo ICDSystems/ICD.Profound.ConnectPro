19233b3ac0ef8be271add4a557e9c269a7d29b0d
Jeffery Thompson <jeffery.thompson@wedoresi.com>
2019-12-09 12:17:27 -0500

    chore: delete unused room combine views/presenters

--- ICD.Profound.ConnectPRO/Themes/TouchDisplayInterface/IPresenters/Settings/RoomCombine/ISettingsRoomCombinePresenter.cs
+++
@@ -1,9 +0,0 @@
-﻿using ICD.Profound.ConnectPRO.SettingsTree.RoomCombine;
-using ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.IViews.Settings.RoomCombine;
-
-namespace ICD.Profound.ConnectPRO.Themes.TouchDisplayInterface.IPresenters.Settings.RoomCombine
-{
-	public interface ISettingsRoomCombinePresenter : ISettingsNodeBasePresenter<ISettingsRoomCombineView, GridSettingsLeaf>
-	{
-	}
-}
