1eedeb23637e72fcc264b70d72ce1e5a7ce06e35
Jeffery Thompson <jeffery.thompson@icdsystems.com>
2020-01-30 16:38:26 -0500

    chore: s# csproj

--- ICD.Profound.ConnectPRO/Themes/TouchDisplayInterface/Presenters/TouchCueIcons.cs
+++ ICD.Profound.ConnectPRO/Themes/TouchDisplayInterface/Presenters/TouchCueIcons.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using ICD.Common.Utils;
+using ICD.Common.Utils.Extensions;
 using ICD.Connect.Routing.Connections;
 using ICD.Connect.Routing.Endpoints.Sources;
 using ICD.Profound.ConnectPRO.Routing.Endpoints.Sources;
