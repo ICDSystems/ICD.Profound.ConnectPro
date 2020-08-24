6056debffa334f036ba2f13f0fd9903686c7545e
Chris Cameron <chris.cameron@wedoresi.com>
2020-08-24 16:44:29 -0400

    chore: Updating changelog, incrementing patch version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [9.1.1] - 2020-08-24
 ### Changed
  - Fixed potential deadlocks in ConnectPro routing
 
--- ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
+++ ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Profound.ConnectPRO")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Profound.ConnectPRO")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("9.1.0.0")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyVersion("9.1.1.0")]
 
 [assembly: AssemblyInformationalVersion("1.3.0")]
 #if SIMPLSHARP
