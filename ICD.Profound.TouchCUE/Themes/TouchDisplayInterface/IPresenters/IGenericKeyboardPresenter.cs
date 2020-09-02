6616ca8e9304bd9b3bc3cafd611c132e38df8a6a
Laura Gomez <laura.gomez@profound-tech.com>
2020-09-02 11:03:19 -0400

    refactor: renamed IContactKeyboard and moved it out of Contacts to allow usage in multiple classes.

---
+++ ICD.Profound.TouchCUE/Themes/TouchDisplayInterface/IPresenters/IGenericKeyboardPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ICD.Profound.TouchCUE.Themes.TouchDisplayInterface.IViews;
+
+namespace ICD.Profound.TouchCUE.Themes.TouchDisplayInterface.IPresenters
+{
+	public interface IGenericKeyboardPresenter : ITouchDisplayPresenter<IGenericKeyboardView>
+	{
+		/// <summary>
+		/// Shows the view using the given callback for the enter button.
+		/// </summary>
+		/// <param name="prompt"></param>
+		/// <param name="text"></param>
+		/// <param name="enterCallback"></param>
+		/// <param name="closeCallback"></param>
+		/// <param name="changeCallback"></param>
+		void ShowView(string prompt, string text, Action<string> enterCallback, Action<string> closeCallback,
+					  Action<string> changeCallback);
+	}
+}
