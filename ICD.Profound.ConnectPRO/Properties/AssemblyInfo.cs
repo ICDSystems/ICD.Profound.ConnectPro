1a8229c023452ed43312ce43f1de31c334277fb2
Chris Cameron <chris.cameron@wedoresi.com>
2020-09-17 12:24:26 -0400

    chore: Updating changelog, incrementing patch version

--- ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
+++ ICD.Profound.ConnectPRO/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Profound.ConnectPRO")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("9.1.1.0")]
+[assembly: AssemblyVersion("9.1.2.0")]
 
 [assembly: AssemblyInformationalVersion("1.3.0")]
 #if SIMPLSHARP
